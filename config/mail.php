92acc1394048d438c5fc732f2742aca2cb140c5b
sykim123 <sykim@yongsanzip.com>
2019-03-05 15:26:36 +0900

    사용자 인증

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => 'john@example.com', 'name' => 'Admin',
+        //'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,36 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', 'WelcomeController@index');
 
+
+Route::get('auth/login', function() {
+    $credentials = [
+        'email' => 'john@example.com',
+        'password' => 'password'
+    ];
+
+    if(! auth()->attempt($credentials)) {
+        return '로그인 정보가 정확하지 않습니다.';
+    }
+    return redirect('protected');
+});
+
+Route::get('protected',['middleware'=>'auth', function() {
+    dump(session()->all());
+
+    return '어서오세요' . auth()->user()->name;
+}]);
+
+Route::get('auth/logout', function() {
+    auth()->logout();
+
+    return '또 봐요~';
+});
+
 Route::resource('articles', 'ArticlesController');
+Auth::routes();
+
+
+
+Route::get('/home', 'HomeController@index')->name('home');
