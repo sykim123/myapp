620ea5bc7ef2461df13d71a66021a9d70f3a9f9f
sykim123 <sykim@yongsanzip.com>
2019-03-05 17:36:20 +0900

    엘로퀀트 ORM

--- app/User.php
+++ app/User.php
@@ -10,6 +10,8 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+
+
     /**
      * The attributes that are mass assignable.
      *
@@ -36,4 +38,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function articles() {
+        return $this->hasMany(Article::class);
+    }
+
 }
