55fefc036b8430983ca15a7bd3370902ffcdf54f
sykim123 <sykim@yongsanzip.com>
2019-03-05 09:30:20 +0900

    라우팅

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [
+    'as' => 'home',
+    function () {
+    return '제 이름은 "home" 입니다.';
+    }
+]);
+
+Route::get('/home', function() {
+    return redirect(route('home'));
 });
+
